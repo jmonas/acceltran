64fe026591f820dfd949198fcb0cb6acb5a5e6dd
Jack Monas <jmonas@princeton.edu>
2023-11-27 22:55:13 -0500

    change processing to one pixel per clock edge

--- synthesis/top/patchifier.sv
+++ synthesis/top/patchifier.sv
@@ -16,7 +16,11 @@ parameter PIXEL_WIDTH = CHANNEL_SIZE * NUM_CHANNELS;
 
 parameter IMG_WIDTH = 64;
 parameter IMG_HEIGHT = 64;
-parameter PATCH_SIZE = 4;
+
+
+parameter PATCH_SIZE = 16;
+parameter PATCH_SIZE_LOG2 = $clog2(PATCH_SIZE); 
+
 parameter PATCHES_IN_ROW = IMG_WIDTH/PATCH_SIZE;
 
 parameter TOTAL_NUM_PATCHES = (IMG_WIDTH/PATCH_SIZE) * (IMG_HEIGHT/PATCH_SIZE);
@@ -34,29 +38,44 @@ output logic [1:0] state;
 output logic [PIXEL_WIDTH-1:0] all_patches [TOTAL_NUM_PATCHES-1:0][PATCH_VECTOR_SIZE-1:0];
 
 
+logic [PIXEL_WIDTH-1:0] reg_image_cache [IMG_WIDTH-1:0][IMG_HEIGHT-1:0];
 logic [PIXEL_WIDTH-1:0] reg_all_patches [TOTAL_NUM_PATCHES-1:0][PATCH_VECTOR_SIZE-1:0];
-logic done;
 
 
+localparam IDLE = 2'b00, PROCESSING = 2'b01, DONE = 2'b10;
 
+reg start_processing = 0; // Flag to start processing
+reg processing_done = 0; // Flag to indicate processing is done
+
+// State Management Block
 always_ff @(posedge clk) begin
 	if (reset) begin
-		state <= 2'b00;
+		state <= IDLE;
 	end
 	else begin
-		if (state == 2'b00 && en) begin
-			state <= 2'b01;
-		end
-		if (state == 2'b01 && done) begin
-			state <= 2'b10;
-		end
-		if (state == 2'b10 && output_taken) begin
-			state <= 2'b00;
-		end
+		case (state)
+			IDLE: if (en || start_processing) state <= PROCESSING;
+			PROCESSING: if (processing_done) state <= DONE;
+			DONE: if (output_taken) state <= IDLE;
+		endcase
 	end
 end
 
 
+
+int p,q;
+always_ff @(posedge clk) begin
+	if (reset || (state == DONE && output_taken)) begin
+		for (p = 0; p < TOTAL_NUM_PATCHES; p++) begin
+			for (q = 0; q < PATCH_VECTOR_SIZE; q++) begin
+				reg_image_cache[p][q] <= 0;
+			end
+		end	
+	end
+	else if (state == IDLE && en) begin
+		reg_image_cache <= image_cache;
+	end
+end
 
 /*
 PATCHIFICATION EXAMPLE:
@@ -67,7 +86,7 @@ PATCHIFICATION EXAMPLE:
     [E][F][*][H] < patch_row_index
     [I][J][K][L]
     [M][N][O][P]
-            ^
+           ^
     patch_col_index
 
 
@@ -86,50 +105,44 @@ PATCHIFICATION EXAMPLE:
     position_index = * = 255
 */
 
-// int i, j;
-// int patch_row_index, patch_col_index, patch_index;
-// int position_row_index, position_col_index, position_index;
-// always_ff @(posedge clk) begin
-// 	if (state == 2'b01) begin
-// 		for (i = 0; i < IMG_WIDTH; i++) begin
-//             patch_row_index <= i/PATCH_SIZE * PATCHES_IN_ROW;
-//             position_row_index <= i%PATCH_SIZE * PATCH_SIZE;
-// 			for (j = 0; j < IMG_HEIGHT; j++) begin
-//                 patch_col_index <= j/PATCH_SIZE;
-//                 position_col_index <= j%PATCH_SIZE;
+int i, j;
+int patch_index, position_index;
+always_ff @(posedge clk) begin
+    if (reset) begin
+        i <= 0;
+        j <= 0;
+        processing_done <= 0;
+    end else if (state == PROCESSING && !processing_done) begin
+        patch_index <= (i >> PATCH_SIZE_LOG2) * PATCHES_IN_ROW + (j >> PATCH_SIZE_LOG2);
+        position_index <= (i & (PATCH_SIZE - 1)) * PATCH_SIZE + (j & (PATCH_SIZE - 1));
 
-//                 patch_index <= patch_row_index + patch_col_index;
-//                 position_index <= position_row_index + position_col_index;
+        reg_all_patches[patch_index][position_index] <= reg_image_cache[i][j];
+        // Increment j, and if it rolls over, increment i
+        j <= j + 1;
+        if (j == IMG_HEIGHT - 1) begin
+            j <= 0;
+            i <= i + 1;
+            if (i == IMG_WIDTH - 1)
+                processing_done <= DONE;
+        end
+    end else if (state != PROCESSING) begin
+        processing_done <= 0; // Reset the flag when not processing
+    end
+end
 
-//                 // assign image pixel value to 1D vector position in corresponding patch
-// 				reg_all_patches[patch_index][position_index] <= image_cache[i][j];
-// 			end
-// 		end	
-// 	end
-// end
 
 
 int l,m;
 always_ff @(posedge clk) begin
-	if (reset || (state == 2'b10 && output_taken)) begin
+	if (reset || (state == DONE && output_taken)) begin
 		for (l = 0; l < TOTAL_NUM_PATCHES; l++) begin
 			for (m = 0; m < PATCH_VECTOR_SIZE; m++) begin
 				all_patches[l][m] <= 0;
 			end
 		end	
 	end
-	else if (state == 2'b01 && done) begin
+	else if (state == DONE) begin
 		all_patches <= reg_all_patches;
-	end
-end
-
-
-always_comb begin
-	if (state == 2'b01) begin
-		done = 1;
-	end
-	else begin
-		done = 0;
 	end
 end
 
