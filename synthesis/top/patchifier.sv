3047becb3b020ea8042789c20c7fdb368f0a6f9e
Jack Monas <jmonas@princeton.edu>
2023-11-28 18:09:09 -0500

    -

--- synthesis/top/patchifier.sv
+++ synthesis/top/patchifier.sv
@@ -1,4 +1,4 @@
-module patchifier
+module patchifier_8
 (
     clk,
     reset, 
@@ -13,14 +13,14 @@ module patchifier
 parameter CHANNEL_SIZE = 8;  
 parameter NUM_CHANNELS = 3; // i.e. RGB
 parameter PIXEL_WIDTH = CHANNEL_SIZE * NUM_CHANNELS; 
-parameter size = 16;
-parameter PATCH_VECTOR_SIZE = size*size; 
+parameter PATCH_SIZE = 8;
+parameter PATCH_VECTOR_SIZE = PATCH_SIZE*PATCH_SIZE; 
 
 input clk, reset;
 input en;
 input output_taken;
 
-input [PIXEL_WIDTH-1:0] patch_cache [size-1:0][size-1:0];
+input [PIXEL_WIDTH-1:0] patch_cache [PATCH_SIZE-1:0][PATCH_SIZE-1:0];
 
 output logic [1:0] state;
 
@@ -28,7 +28,7 @@ output logic [1:0] state;
 output logic [PIXEL_WIDTH-1:0] vectorized_patch [PATCH_VECTOR_SIZE-1:0];
 
 
-logic [PIXEL_WIDTH-1:0] reg_patch_cache [size-1:0][size-1:0];
+logic [PIXEL_WIDTH-1:0] reg_patch_cache [PATCH_SIZE-1:0][PATCH_SIZE-1:0];
 logic [PIXEL_WIDTH-1:0] reg_vectorized_patch [PATCH_VECTOR_SIZE-1:0];
 logic processing_done; // Flag to indicate processing is done
 
@@ -56,8 +56,8 @@ end
 int p, q;
 always_ff @(posedge clk) begin
 	if (reset || (state == DONE && output_taken)) begin
-		for (p = 0; p < size; p++) begin
-			for (q = 0; q < size; q++) begin
+		for (p = 0; p < PATCH_SIZE; p++) begin
+			for (q = 0; q < PATCH_SIZE; q++) begin
 				reg_patch_cache[p][q] <= 0;
 			end
 		end	
@@ -100,14 +100,14 @@ always_ff @(posedge clk) begin
         j <= 0;
         processing_done <= 0;
     end else if (state == PROCESSING && !processing_done) begin
-        position_index <= i * size + j;
+        position_index <= i * PATCH_SIZE + j;
         reg_vectorized_patch[position_index] <= reg_patch_cache[i][j];
         // Increment j, and if it rolls over, increment i
         j <= j + 1;
-        if (j == size - 1) begin
+        if (j == PATCH_SIZE - 1) begin
             j <= 0;
             i <= i + 1;
-            if (i == size - 1)
+            if (i == PATCH_SIZE - 1)
                 processing_done <= 1;
         end
     end else if (state != PROCESSING) begin
