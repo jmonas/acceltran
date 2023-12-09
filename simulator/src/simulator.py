f78d2fc000dcb8eff4e2739fba29e2fa3e04b77d
Jack Monas <jmonas@princeton.edu>
2023-12-09 15:23:57 -0500

    -

--- simulator/src/simulator.py
+++ simulator/src/simulator.py
@@ -339,6 +339,7 @@ def simulate(model_dict: dict, config: dict, constants: dict, design_space: dict
 	print(transformer_type)
 	memory_ops, compute_ops, num_ops = dict2ops(model_dict, config, mode=mode, tile_compute_ops=config['scheduler']['compute_ops']['tiled'], tile_memory_ops=config['scheduler']['memory_ops']['tiled'], first_layer_only=first_layer_only, debug=debug, transformer_type = transformer_type)
 
+	print(compute_ops)
 	assert type(memory_ops[1]) == list and type(compute_ops[0]) == list
 	memory_op_idx, compute_op_idx, ops_done = [0, []], [0, [0] * len(compute_ops[0])], 0
 
