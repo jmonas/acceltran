cc717d3ccd604148f731436f694eeccfe8b48c77
Jack Monas <jmonas@princeton.edu>
2024-04-05 00:39:56 -0400

    a

--- training/ViLT/test/inference.py
+++ training/ViLT/test/inference.py
@@ -21,7 +21,7 @@ from PIL import Image
 
 url = "https://www.realmenrealstyle.com/wp-content/uploads/2023/11/The-Porkpie-hat-stubble.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
-text = "Is this a man or woman?"
+text = "Man or woman?"
 
 config = json.load(open('config_medium_plus.json'))
 
