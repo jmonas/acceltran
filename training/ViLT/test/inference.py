da91698055bd33bb36ed793f743019c926f6e04d
Jack Monas <jmonas@princeton.edu>
2024-04-04 20:59:44 -0400

    a

--- training/ViLT/test/inference.py
+++ training/ViLT/test/inference.py
@@ -21,7 +21,7 @@ from PIL import Image
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
-text = "what animal is in the picture?"
+text = "Is there a cat?"
 
 config = json.load(open('config.json'))
 
