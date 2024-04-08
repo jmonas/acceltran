f56005fc6f7c8f9bb50af664ad495fc670a003e1
Jack Monas <jmonas@princeton.edu>
2024-04-07 21:32:28 -0400

    model

---
+++ training/ViLT/test/upload_model_adroit.py
@@ -0,0 +1,8 @@
+from transformers import ViltProcessor, ViltForQuestionAnswering, ViltConfig
+import json
+import requests
+
+
+config = json.load(open('config_small.json'))
+configuration = ViltConfig(**config)
+model =ViltForQuestionAnswering.from_pretrained("jmonas/ViLT-5M-vqa", config=configuration, use_safetensors=True, cache = "/scratch/network/jmonas")
