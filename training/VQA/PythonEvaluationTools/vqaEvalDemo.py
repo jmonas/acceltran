7a074f5968ec8512d48cc63891f533184b6251d6
Jack Monas <jmonas@princeton.edu>
2024-04-08 03:10:39 -0400

    eval

--- training/VQA/PythonEvaluationTools/vqaEvalDemo.py
+++ training/VQA/PythonEvaluationTools/vqaEvalDemo.py
@@ -11,79 +11,77 @@ import json
 import random
 import os
 
-# set up file names and paths
-versionType ='v2_' # this should be '' when using VQA v2.0 dataset
-taskType    ='OpenEnded' # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
-dataType    ='mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0. 
-dataSubType ='test2015'
-annFile     ='/home/jmonas/acceltran/training/ViLT/test/vqa_predictions.json'
-quesFile    ='/scratch/gpfs/jmonas/VQA/v2_OpenEnded_mscoco_test-dev2015_questions.json'
-imgDir      ='/scratch/gpfs/jmonas/VQA/test2015'
-resultType  ='fake'
-fileTypes   = ['results', 'accuracy', 'evalQA', 'evalQuesType', 'evalAnsType'] 
+def get_accuracy(annFile, resFile, quesFile):
 
-# An example result json file has been provided in './Results' folder.  
+	# annFile     ='/home/jmonas/acceltran/training/ViLT/test/vqa_predictions.json'
+	# resFile     ='/home/jmonas/acceltran/training/ViLT/test/vqa_predictions.json'
+	# quesFile    ='/scratch/gpfs/jmonas/VQA/v2_OpenEnded_mscoco_test-dev2015_questions.json'
+	# imgDir      ='/scratch/gpfs/jmonas/VQA/test2015'
+	# resultType  ='fake'
+	# fileTypes   = ['results', 'accuracy', 'evalQA', 'evalQuesType', 'evalAnsType'] 
 
-[resFile, accuracyFile, evalQAFile, evalQuesTypeFile, evalAnsTypeFile] = ['%s/Results/%s%s_%s_%s_%s_%s.json'%(dataDir, versionType, taskType, dataType, dataSubType, \
-resultType, fileType) for fileType in fileTypes]  
+	# An example result json file has been provided in './Results' folder.  
 
-# create vqa object and vqaRes object
-vqa = VQA(annFile, quesFile)
-vqaRes = vqa.loadRes(resFile, quesFile)
+	# [resFile, accuracyFile, evalQAFile, evalQuesTypeFile, evalAnsTypeFile] = ['%s/Results/%s%s_%s_%s_%s_%s.json'%(dataDir, versionType, taskType, dataType, dataSubType, \
+	# resultType, fileType) for fileType in fileTypes]  
 
-# create vqaEval object by taking vqa and vqaRes
-vqaEval = VQAEval(vqa, vqaRes, n=2)   #n is precision of accuracy (number of places after decimal), default is 2
+	# create vqa object and vqaRes object
+	vqa = VQA(annFile, quesFile)
+	vqaRes = vqa.loadRes(resFile, quesFile)
 
-# evaluate results
-"""
-If you have a list of question ids on which you would like to evaluate your results, pass it as a list to below function
-By default it uses all the question ids in annotation file
-"""
-vqaEval.evaluate() 
+	# create vqaEval object by taking vqa and vqaRes
+	vqaEval = VQAEval(vqa, vqaRes, n=2)   #n is precision of accuracy (number of places after decimal), default is 2
 
-# print accuracies
-print("\n")
-print("Overall Accuracy is: %.02f\n" %(vqaEval.accuracy['overall']))
-print("Per Question Type Accuracy is the following:")
-for quesType in vqaEval.accuracy['perQuestionType']:
-	print("%s : %.02f" %(quesType, vqaEval.accuracy['perQuestionType'][quesType]))
-print( "\n")
-print( "Per Answer Type Accuracy is the following:")
-for ansType in vqaEval.accuracy['perAnswerType']:
-	print( "%s : %.02f" %(ansType, vqaEval.accuracy['perAnswerType'][ansType]))
-print( "\n")
-# # demo how to use evalQA to retrieve low score result
-# evals = [quesId for quesId in vqaEval.evalQA if vqaEval.evalQA[quesId]<35]   #35 is per question percentage accuracy
-# if len(evals) > 0:
-# 	print('ground truth answers')
-# 	randomEval = random.choice(evals)
-# 	randomAnn = vqa.loadQA(randomEval)
-# 	vqa.showQA(randomAnn)
+	# evaluate results
+	"""
+	If you have a list of question ids on which you would like to evaluate your results, pass it as a list to below function
+	By default it uses all the question ids in annotation file
+	"""
+	vqaEval.evaluate() 
 
-# 	print( '\n')
-# 	print('generated answer (accuracy %.02f)'%(vqaEval.evalQA[randomEval]))
-# 	ann = vqaRes.loadQA(randomEval)[0]
-# 	print( "Answer:   %s\n" %(ann['answer']))
+	# print accuracies
+	print("\n")
+	print("Overall Accuracy is: %.02f\n" %(vqaEval.accuracy['overall']))
+	print("Per Question Type Accuracy is the following:")
+	for quesType in vqaEval.accuracy['perQuestionType']:
+		print("%s : %.02f" %(quesType, vqaEval.accuracy['perQuestionType'][quesType]))
+	print( "\n")
+	print( "Per Answer Type Accuracy is the following:")
+	for ansType in vqaEval.accuracy['perAnswerType']:
+		print( "%s : %.02f" %(ansType, vqaEval.accuracy['perAnswerType'][ansType]))
+	print( "\n")
+	# # demo how to use evalQA to retrieve low score result
+	# evals = [quesId for quesId in vqaEval.evalQA if vqaEval.evalQA[quesId]<35]   #35 is per question percentage accuracy
+	# if len(evals) > 0:
+	# 	print('ground truth answers')
+	# 	randomEval = random.choice(evals)
+	# 	randomAnn = vqa.loadQA(randomEval)
+	# 	vqa.showQA(randomAnn)
 
-# 	imgId = randomAnn[0]['image_id']
-# 	imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
-# 	if os.path.isfile(imgDir + imgFilename):
-# 		I = io.imread(imgDir + imgFilename)
-# 		plt.imshow(I)
-# 		plt.axis('off')
-# 		plt.show()
+	# 	print( '\n')
+	# 	print('generated answer (accuracy %.02f)'%(vqaEval.evalQA[randomEval]))
+	# 	ann = vqaRes.loadQA(randomEval)[0]
+	# 	print( "Answer:   %s\n" %(ann['answer']))
 
-# # plot accuracy for various question types
-# plt.bar(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].values(), align='center')
-# plt.xticks(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].keys(), rotation='0',fontsize=10)
-# plt.title('Per Question Type Accuracy', fontsize=10)
-# plt.xlabel('Question Types', fontsize=10)
-# plt.ylabel('Accuracy', fontsize=10)
-# plt.show()
+	# 	imgId = randomAnn[0]['image_id']
+	# 	imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
+	# 	if os.path.isfile(imgDir + imgFilename):
+	# 		I = io.imread(imgDir + imgFilename)
+	# 		plt.imshow(I)
+	# 		plt.axis('off')
+	# 		plt.show()
 
-# # save evaluation results to ./Results folder
-# json.dump(vqaEval.accuracy,     open(accuracyFile,     'w'))
-# json.dump(vqaEval.evalQA,       open(evalQAFile,       'w'))
-# json.dump(vqaEval.evalQuesType, open(evalQuesTypeFile, 'w'))
-# json.dump(vqaEval.evalAnsType,  open(evalAnsTypeFile,  'w'))
+	# # plot accuracy for various question types
+	# plt.bar(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].values(), align='center')
+	# plt.xticks(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].keys(), rotation='0',fontsize=10)
+	# plt.title('Per Question Type Accuracy', fontsize=10)
+	# plt.xlabel('Question Types', fontsize=10)
+	# plt.ylabel('Accuracy', fontsize=10)
+	# plt.show()
+
+	# # save evaluation results to ./Results folder
+	# json.dump(vqaEval.accuracy,     open(accuracyFile,     'w'))
+	# json.dump(vqaEval.evalQA,       open(evalQAFile,       'w'))
+	# json.dump(vqaEval.evalQuesType, open(evalQuesTypeFile, 'w'))
+	# json.dump(vqaEval.evalAnsType,  open(evalAnsTypeFile,  'w'))
 
--- training/ViLT/test/eval.py
+++ training/ViLT/test/eval.py
@@ -8,120 +8,153 @@ import re
 import torch
 from typing import Optional
 from tqdm.auto import tqdm
+import os
 from os import listdir
 from os.path import isfile, join
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import argparse
+import datetime
+import sys 
+sys.path.insert(0, '../../VQA/PythonEvaluationTools')
+from vqaEvalDemo import get_accuracy
 
-DELLA = False
-config = json.load(open('config_small2.json'))
-size = "l2_h256_i512"
-cache_dir = "/scratch/gpfs/jmonas" if DELLA else "/scratch/network/jmonas"
-model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-1-1" if DELLA else f"jmonas/ViLT-11M-vqa"
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-configuration = ViltConfig(**config)
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa", cache_dir=cache_dir)
-model =ViltForQuestionAnswering.from_pretrained(model_location, config=configuration, use_safetensors=True, cache_dir=cache_dir)
-
-
-# Opening JSON file
-f = open(f'{cache_dir}/VQA/v2_OpenEnded_mscoco_test2015_questions.json')
-
-# Return JSON object as dictionary
-questions = json.load(f)['questions']
-
-filename_re = re.compile(r".*(\d{12})\.((jpg)|(png))")
-
-# source: https://github.com/allenai/allennlp-models/blob/a36aed540e605c4293c25f73d6674071ca9edfc3/allennlp_models/vision/dataset_readers/vqav2.py#L141
-def id_from_filename(filename: str) -> Optional[int]:
-    match = filename_re.fullmatch(filename)
-    if match is None:
-        return None
-    return int(match.group(1))
-
-root = f'{cache_dir}/VQA/test2015'
-file_names = [f for f in tqdm(listdir(root)) if isfile(join(root, f))]
-
-filename_to_id = {root + "/" + file: id_from_filename(file) for file in file_names}
-id_to_filename = {v:k for k,v in filename_to_id.items()}
-
-class VQADataset(torch.utils.data.Dataset):
-    """VQA (v2) dataset."""
-
-    def __init__(self, questions, processor):
-        self.questions = questions
-        self.processor = processor
-        self.transform = transforms.Compose([
-            transforms.Lambda(lambda img: img.convert('RGB')),
-        ])
-
-    def __len__(self):
-        return len(self.questions)
-
-    def __getitem__(self, idx):
-        # get image + text
-        question = self.questions[idx]
-        image_path = id_to_filename[question['image_id']]
-        with Image.open(image_path) as img:
-            # Convert any image to RGB (3 channels)
-            image = self.transform(img)
-
-        text = question['question']
-
-        encoding = self.processor(image, text, padding="max_length", truncation=True, return_tensors="pt")
-        
-        encoding["question_id"] = question["question_id"]
-        return encoding
+def eval (config_file, questions_file, images_dir, batch_size = 32, annFile = None):
+    config = json.load(open(config_file))
+    size = f"l{config["num_hidden_layers"]}_h{config["hidden_size"]}_i{config["intermediate_size"]}"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    configuration = ViltConfig(**config)
+    processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa", cache_dir=cache_dir)
+    model =ViltForQuestionAnswering.from_pretrained(model_location, config=configuration, use_safetensors=True, cache_dir=cache_dir)
 
 
+    # Opening JSON file
+    f = open(questions_file)
+
+    # Return JSON object as dictionary
+    questions = json.load(f)['questions']
+
+    filename_re = re.compile(r".*(\d{12})\.((jpg)|(png))")
+
+    # source: https://github.com/allenai/allennlp-models/blob/a36aed540e605c4293c25f73d6674071ca9edfc3/allennlp_models/vision/dataset_readers/vqav2.py#L141
+    def id_from_filename(filename: str) -> Optional[int]:
+        match = filename_re.fullmatch(filename)
+        if match is None:
+            return None
+        return int(match.group(1))
+    
+    file_names = [f for f in tqdm(listdir(images_dir)) if isfile(join(images_dir, f))]
+
+    filename_to_id = {images_dir + "/" + file: id_from_filename(file) for file in file_names}
+    id_to_filename = {v:k for k,v in filename_to_id.items()}
+
+    class VQADataset(torch.utils.data.Dataset):
+        """VQA (v2) dataset."""
+
+        def __init__(self, questions, processor):
+            self.questions = questions
+            self.processor = processor
+            self.transform = transforms.Compose([
+                transforms.Lambda(lambda img: img.convert('RGB')),
+            ])
+
+        def __len__(self):
+            return len(self.questions)
+
+        def __getitem__(self, idx):
+            # get image + text
+            question = self.questions[idx]
+            image_path = id_to_filename[question['image_id']]
+            with Image.open(image_path) as img:
+                # Convert any image to RGB (3 channels)
+                image = self.transform(img)
+
+            text = question['question']
+
+            encoding = self.processor(image, text, padding="max_length", truncation=True, return_tensors="pt")
+            
+            encoding["question_id"] = question["question_id"]
+            return encoding
 
 
-def collate_fn(batch):
-  input_ids = [item['input_ids'][0] for item in batch]
-  pixel_values = [item['pixel_values'][0] for item in batch]
-  token_type_ids = [item['token_type_ids'][0] for item in batch]
-  question_ids = [item['question_id'] for item in batch]
 
-  # create padded pixel values and corresponding pixel mask
-  encoding = processor.image_processor.pad(pixel_values, return_tensors="pt")
 
-  # create new batch
-  batch = {}
-  batch['input_ids'] = torch.stack(input_ids)
-  batch['question_ids'] = torch.tensor(question_ids)
-  batch['token_type_ids'] = torch.stack(token_type_ids)
-  batch['pixel_values'] = encoding['pixel_values']
-  batch['pixel_mask'] = encoding['pixel_mask']
+    def collate_fn(batch):
+        input_ids = [item['input_ids'][0] for item in batch]
+        pixel_values = [item['pixel_values'][0] for item in batch]
+        token_type_ids = [item['token_type_ids'][0] for item in batch]
+        question_ids = [item['question_id'] for item in batch]
 
-  return batch
+        # create padded pixel values and corresponding pixel mask
+        encoding = processor.image_processor.pad(pixel_values, return_tensors="pt")
 
-batch_size = 32
-print(len(questions))
-test_dataset = VQADataset(questions=questions,
-                           processor=processor)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True,collate_fn=collate_fn)
+        # create new batch
+        batch = {}
+        batch['input_ids'] = torch.stack(input_ids)
+        batch['question_ids'] = torch.tensor(question_ids)
+        batch['token_type_ids'] = torch.stack(token_type_ids)
+        batch['pixel_values'] = encoding['pixel_values']
+        batch['pixel_mask'] = encoding['pixel_mask']
 
-model.eval()  # Set the model to evaluation mode
-model.to(device)
+        return batch
 
-predictions = []
+    test_dataset = VQADataset(questions=questions,
+                            processor=processor)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True,collate_fn=collate_fn)
 
-with torch.no_grad():
-    for idx, batch in enumerate(test_dataloader):
-        print(idx, flush=True)
-        # Adapt these lines based on how your DataLoader and model are set up
-        inputs = {'pixel_values': batch['pixel_values'].to(device), 'input_ids': batch['input_ids'].to(device)}
-        outputs = model(**inputs)
-        preds = outputs.logits.argmax(-1).tolist()  # Convert logits to predicted indices
-        
-        for question, pred in zip(batch["question_ids"], preds):
-            # Convert `pred` to the corresponding answer string. This may involve a mapping similar to `id2label`.
-            answer = config["id2label"][str(pred)]  # This is a placeholder; adapt it to your model's specifics
-            predictions.append({'question_id': question.item(), 'answer': answer})
+    model.eval()  # Set the model to evaluation mode
+    model.to(device)
 
-# Save predictions to a JSON file
-with open(f'vqa_predictions_{size}_round_3.json', 'w') as f:
-    json.dump(predictions, f)
+    predictions = []
 
+    with torch.no_grad():
+        for idx, batch in enumerate(test_dataloader):
+            print(idx, flush=True)
+            # Adapt these lines based on how your DataLoader and model are set up
+            inputs = {'pixel_values': batch['pixel_values'].to(device), 'input_ids': batch['input_ids'].to(device)}
+            outputs = model(**inputs)
+            preds = outputs.logits.argmax(-1).tolist()  # Convert logits to predicted indices
+            
+            for question, pred in zip(batch["question_ids"], preds):
+                # Convert `pred` to the corresponding answer string. This may involve a mapping similar to `id2label`.
+                answer = config["id2label"][str(pred)]  # This is a placeholder; adapt it to your model's specifics
+                predictions.append({'question_id': question.item(), 'answer': answer})
+
+    current_date = datetime.datetime.now().strftime("%Y%m%d")
+    predictions_file = f'vqa_predictions_{size}_{current_date}.json'
+    with open(predictions_file, 'w') as f:
+        json.dump(predictions, f)
+    
+    if annFile:
+        pwd = os.getcwd()
+        predictions_with_path = os.path.join(pwd, predictions_file)
+        get_accuracy(annFile, predictions_with_path, questions_file)
+    
+    
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Set DELLA and TEST parameters.')
+    parser.add_argument('--della', action='store_false', dest='della', help='Enable DELLA parameter')
+    parser.add_argument('--test', action='store_false', dest='test', help='Disable TEST parameter')
+
+    args = parser.parse_args()
+
+    DELLA = args.della
+    TEST = args.test
+
+    config_file = 'config_medium_plus.json'
+    size = "l6_h512_i1024"
+    cache_dir = "/scratch/gpfs/jmonas" if DELLA else "/scratch/network/jmonas"
+    questions_type = "v2_OpenEnded_mscoco_test2015_questions.json" if TEST else "v2_OpenEnded_mscoco_val2014_questions.json"
+    images_type = "test2015" if TEST else "val2014"
+    model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-93-0" if DELLA else f"jmonas/ViLT-11M-vqa"
+    questions_file= f'{cache_dir}/VQA/{questions_type}'
+    images_dir = f'{cache_dir}/VQA/{images_type}'
+
+    if TEST:
+        eval(config_file, questions_file, images_dir, 32, DELLA, TEST,)
+    # get validation proxy accuracy
+    else: 
+        annFile =f'{cache_dir}/VQA/v2_mscoco_val2014_annotations.json'
+        eval(config_file, questions_file, images_dir, 32, DELLA, TEST, annFile)
