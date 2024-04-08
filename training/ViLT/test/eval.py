d6d8aa94d1be3aa2e519596f55f84a96a5d34ade
Jack Monas <jmonas@princeton.edu>
2024-04-07 22:34:08 -0400

    a

--- training/ViLT/test/eval.py
+++ training/ViLT/test/eval.py
@@ -14,10 +14,10 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 DELLA = False
-config = json.load(open('config_medium_plus.json'))
+config = json.load(open('config_medium.json'))
 size = "l6_h512_i1024"
 cache_dir = "/scratch/gpfs/jmonas" if DELLA else "/scratch/network/jmonas"
-model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-1-1" if DELLA else f"jmonas/ViLT-33M-vqa"
+model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-1-1" if DELLA else f"jmonas/ViLT-12M-vqa"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
