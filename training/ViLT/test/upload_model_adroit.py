fc39f9383e42dfebfe03e88d28d677f067a654c5
Jack Monas <jmonas@princeton.edu>
2024-04-07 21:39:03 -0400

    a

--- training/ViLT/test/upload_model_adroit.py
+++ training/ViLT/test/upload_model_adroit.py
@@ -5,4 +5,4 @@ import requests
 
 config = json.load(open('config_small.json'))
 configuration = ViltConfig(**config)
-model =ViltForQuestionAnswering.from_pretrained("jmonas/ViLT-5M-vqa", config=configuration, use_safetensors=True, cache = "/scratch/network/jmonas")
+model =ViltForQuestionAnswering.from_pretrained("jmonas/ViLT-5M-vqa", config=configuration, use_safetensors=True, cache_dir = "/scratch/network/jmonas")
