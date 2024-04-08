5ae31de845e0d9456697be47d1a0e979bc601d75
Jack Monas <jmonas@princeton.edu>
2024-04-07 22:41:40 -0400

    revert

--- training/ViLT/test/sbmit_eval.sh
+++ training/ViLT/test/sbmit_eval.sh
@@ -4,6 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -12,6 +13,8 @@
 #SBATCH --partition=mig
 
 module purge
+module load cudatoolkit/11.1
+module load cudnn/cuda-11.x/8.2.0
 module load anaconda3/2023.9
 conda activate torch-env
 
