bdf718060e5a84707dd00a372276ebd04afdfe35
Jack Monas <jmonas@princeton.edu>
2024-04-07 21:25:13 -0400

    a

--- training/ViLT/test/upload_to_hugging.py
+++ training/ViLT/test/upload_to_hugging.py
@@ -7,6 +7,6 @@ from PIL import Image
 
 config = json.load(open('config_small.json'))
 configuration = ViltConfig(**config)
-model =ViltForQuestionAnswering.from_pretrained("/scratch/gpfs/jmonas/ViLT/Model/vilt-saved-model-0", config=configuration, use_safetensors=True)
+model =ViltForQuestionAnswering.from_pretrained("/home/jmonas/acceltran/training/ViLT/test/Model/vilt-saved-model-0", config=configuration, use_safetensors=True)
 
 model.push_to_hub("ViLT-5M-vqa")
