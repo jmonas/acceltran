ebd30be0911558d6cc3294d11651968c751be699
Jack Monas <jmonas@princeton.edu>
2024-04-08 19:08:29 -0400

    a

--- training/FLAVA/model.py
+++ training/FLAVA/model.py
@@ -28,20 +28,14 @@ class FlavaForVQA(nn.Module):
         self.vqa_classifier = VQAClassifierHead(flava_model.config.multimodal_config.hidden_size, flava_model.config.multimodal_config.hidden_size, num_labels)
         
     def forward(self, input_ids, pixel_values, attention_mask, labels = None):
-        # Get the multimodal encoder outputs
         outputs = self.flava_model(input_ids=input_ids, pixel_values=pixel_values, attention_mask=attention_mask)
-        # Use the output corresponding to the [CLS_M] token
-        hCLS_M = outputs.last_hidden_state[:, 0]  # assuming that the [CLS_M] token is the first token in the sequence
-        
-        # Pass the [CLS_M] representation through the classifier head
-        logits = self.vqa_classifier(hCLS_M)
+        multimodal_embeddings = outputs.multimodal_embeddings
+        logits = self.vqa_classifier(multimodal_embeddings)
         
         loss = None
         if labels is not None:
-            # move labels to correct device to enable PP
             labels = labels.to(logits.device)
             loss = nn.functional.binary_cross_entropy_with_logits(logits, labels) * labels.shape[1]
-            # see https://github.com/jnhwkim/ban-vqa/blob/master/train.py#L19
 
         return SequenceClassifierOutput(
             loss=loss,
