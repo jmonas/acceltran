4f6b39b8329c8e1a1ff84988efee007b791c958b
Jack Monas <jmonas@princeton.edu>
2024-04-07 21:20:03 -0400

    small2

--- training/ViLT/test/upload_to_hugging.py
+++ training/ViLT/test/upload_to_hugging.py
@@ -5,7 +5,7 @@ import requests
 from PIL import Image
 
 
-config = json.load(open('config_small.json'))
+config = json.load(open('config_small2.json'))
 configuration = ViltConfig(**config)
 model =ViltForQuestionAnswering.from_pretrained("/scratch/gpfs/jmonas/ViLT/Models/l2_h256_i512/vilt-saved-model-ft-97-0", config=configuration, use_safetensors=True)
 
