b29f15ba245f1e268d574dd7101ea0d122d5b308
Jack Monas <jmonas@princeton.edu>
2024-04-08 21:02:45 -0400

    a

--- training/FLAVA/train.py
+++ training/FLAVA/train.py
@@ -206,7 +206,7 @@ def collate_fn(batch):
 
   return batch
 
-batch_size = 32
+batch_size = 16
 
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
