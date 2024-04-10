f5c27e06d8cbff049273d4d6cc148a9a14b00058
Jack Monas <jmonas@princeton.edu>
2024-04-10 08:29:38 -0400

    a

--- training/VQA/PythonEvaluationTools/vqaEvalDemo.py
+++ training/VQA/PythonEvaluationTools/vqaEvalDemo.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import sys
-dataDir = '../../VQA'
+dataDir = '../VQA'
 sys.path.insert(0, '%s/PythonHelperTools/vqaTools' %(dataDir))
 from vqa import VQA
 from vqaEvaluation.vqaEval import VQAEval
