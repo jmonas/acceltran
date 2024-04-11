15598df691d387ff4280a39cca9f6aa498fc31bf
Jack Monas <jmonas@princeton.edu>
2024-04-11 00:58:44 -0400

    a

--- pruning/VLM/run_evaluation.py
+++ pruning/VLM/run_evaluation.py
@@ -10,6 +10,8 @@ from transformers import ViltProcessor, ViltForQuestionAnswering, ViltConfig
 import torch
 sys.path.append('../../txf_design-space/transformers_new/src/transformers')
 sys.path.append("/home/jmonas/acceltran/txf_design-space/transformers_new/transformers/src/transformers/models/vilt/")
+print(sys.path)
+
 from modeling_dtvilt import DTViltModel, DTViltForMaskedLM, DTViltForSequenceClassification, DTViltForQuestionAnswering
 
 USE_NON_PRUNED = False
