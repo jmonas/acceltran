535ad9a73495b858d92de40daff062d00194edc0
Jack Monas <jmonas@princeton.edu>
2024-04-11 19:54:26 -0400

    adroit

--- pruning/VLM/run_evaluation.py
+++ pruning/VLM/run_evaluation.py
@@ -10,10 +10,12 @@ from transformers import ViltProcessor, ViltForQuestionAnswering, ViltConfig
 import torch
 sys.path.append('../../transformers_new/src/transformers')
 sys.path.append("/home/jmonas/acceltran/transformers/src/transformers/models/vilt/")
+sys.path.append("/home/jmonas/Desktop/acceltran/transformers/src/transformers/models/vilt/")
 print(sys.path)
 
 from modeling_dtvilt import DTViltModel, DTViltForQuestionAnswering
 from datetime import datetime
+import time
 
 USE_NON_PRUNED = False
 
@@ -86,11 +88,12 @@ def main (model_info, max_pruning_threshold, min_k, method = "dynatran"):
 
 
 if __name__ == '__main__':
-		config_file = '/home/jmonas/acceltran/training/ViLT/config_medium_plus.json'
+		config_file = '/home/jmonas/acceltran/training/ViLT/small.json'
 		config = json.load(open(config_file))
 		size = f"l{config['num_hidden_layers']}_h{config['hidden_size']}_i{config['intermediate_size']}"
-		cache_dir= "/scratch/gpfs/jmonas"
-		model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-93-0"
+		cache_dir= "/scratch/network/jmonas"
+		# model_location = f"{cache_dir}/ViLT/Models/{size}/vilt-saved-model-ft-93-0"
+		model_location = "jmonas/ViLT-5M-vqa"
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		config = ViltConfig(**config)
 		processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa", cache_dir=cache_dir)
@@ -105,3 +108,51 @@ if __name__ == '__main__':
 			"size":size
 		}
 		main(model_info, .15, None)
+
+
+
+
+
+# def throughput ():
+
+
+
+# 	dp = .05
+# 	top_k = 16
+
+# 	# Set p and k for 30% activation sparsity
+# 	cases = [(dp, None), (0, top_k)]
+# 	output_dir = f'./results/throughput/{model_name}_{size}_VQA'
+# 	os.makedirs(output_dir, exist_ok=True)
+
+# 	results = []
+
+# 	for p, k in cases:
+# 		print(f'Running inference with pruning threshold: {p}, and \'k\': {k}')
+# 		result = {'pruning_threshold': p, 'k': k}
+
+# 		# Make new output directory
+# 		temp_dir = os.path.join(output_dir, f'threshold_p{str(p)[2:]}_k{int(k)}_{datetime.now()}')
+
+# 		# Load and save new config
+# 		config.pruning_threshold = p
+# 		config.k = k
+# 		config.sparsity_file = None
+# 		config.save_pretrained(temp_dir)
+
+# 		# Load model
+# 		model = model_class.from_pretrained(model_location, config=config, use_safetensors=True, cache_dir=cache_dir)
+
+# 		# Run evaluation on the SST-2 task or the SQuAD task
+# 		start_time = time.time()
+# 		metrics = run_glue(training_args)
+# 		end_time = time.time()
+# 		print(metrics)
+
+# 		result['throughput'] = 256 / (end_time - start_time)
+# 		print(f'Throughput: {256 / (end_time - start_time)} seq/sec')
+
+# 		results.append(result)
+# 		json.dump(results, open(os.path.join(output_dir, 'results.json'), 'w+'))
+
+# 	return
