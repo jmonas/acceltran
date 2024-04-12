434525558a562b45a1431b37e500b9034e9677f5
Jack Monas <jmonas@princeton.edu>
2024-04-12 04:23:49 -0400

    a

--- training/FLAVA/fine_tune.py
+++ training/FLAVA/fine_tune.py
@@ -113,12 +113,12 @@ for annotation in tqdm(annotations):
     annotation['scores'] = scores
 
 
-config = json.load(open('config_small.json'))
+config = json.load(open('config_tiny.json'))
 size = f"l{config["uni_layers"]}_h{config["hidden_size"]}_i{config["intermediate_size"]}"
 configuration = config_maker(config["uni_layers"], config["hidden_size"], config["number_heads"], config["intermediate_size"])
 processor = FlavaProcessor.from_pretrained("facebook/flava-full", cache_dir="/scratch/gpfs/jmonas")
 model = FlavaForVQA(configuration, len(id2label))
-model_path = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}_1/flava-saved-model-ft-5-21.pt"
+model_path = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}_1/flava-saved-model-ft-7-23.pt"
 model.load_state_dict(torch.load(model_path))
 
 flava_params = sum(p.numel() for p in model.parameters())
@@ -265,8 +265,8 @@ for epoch in range(num_epochs):
             print("Epoch: {} - Training loss: {} - Eval Loss: {} - LR: {}".format(epoch+1, epoch_loss/len(train_dataloader), eval_loss/len(valid_dataloader), optimizer.param_groups[0]["lr"]), flush=True)
             # scheduler.step()
             if eval_loss < min_eval_loss:
-                save_path = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}_1"
-                model_filename = f"flava-saved-model-ft_v2-{epoch}-{idx//500}.pt"
+                save_path = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}_B"
+                model_filename = f"flava-saved-model-ft_v3-{epoch}-{idx//500}.pt"
                 full_model_path = os.path.join(save_path, model_filename)
                 os.makedirs(save_path, exist_ok=True)
                 torch.save(model.state_dict(), full_model_path)
