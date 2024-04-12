ba6cb084c0408c0cf71eeb52ee4c242342c84cff
Jack Monas <jmonas@princeton.edu>
2024-04-12 06:10:45 -0400

    s

--- pruning/VLM/run_evaluation_flava.py
+++ pruning/VLM/run_evaluation_flava.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
 		cache_dir= "/scratch/gpfs/jmonas"
 		processor = FlavaProcessor.from_pretrained("facebook/flava-full", cache_dir="/scratch/gpfs/jmonas")
 		model_class = DTFlavaForVQA
-		model_location = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}__B/flava-saved-model-ft_v3-0-22.pt"
+		model_location = f"/scratch/gpfs/jmonas/FLAVA/Models/{size}_B/flava-saved-model-ft_v3-0-22.pt"
 
 
 		model_info = {
